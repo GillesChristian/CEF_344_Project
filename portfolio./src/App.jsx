7401c27c80796a3b1803ad03cde2e1f34870b832
GillesChristian <christiank.kamgaing@gmail.com>
2023-05-15 16:38:21 +0100

    Header of Portfolio added with  favicon

--- portfolio./src/App.jsx
+++ portfolio./src/App.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from 'react';
+import './index.css';
+import Header from './components/header/Header';
+import Navbar from './components/navbar/Navbar';
+import About from './components/about/About';
+import Experience from './components/experience/Experience';
+import Services from './components/services/Services';
+import Portfolio from './components/portfolio/Portfolio';
+import Testimonial from './components/testimonial/Testimonial';
+import Contact from './components/contact/Contact';
+import Footer from './components/footer/Footer';
 
-export const App = () => {
+function App() {
+
     return (
 
-        <div>App</div>
+            <>
+                <Header/>
+             
+            </>
 
-    )
-}
+    );    
+}   
 
-export default App;
+ export default App;
---
+++ portfolio./src/components/header/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './header.css'
+import './CTA'
+import CTA from './CTA' 
+import me from '../../assets/gilles.jpg'
+import HeaderSocial from './HeaderSocial' 
+
+export default function Header () {
+
+    return (
+        <header>
+            <div className='container header__container'>
+
+                <h5>Hello I'm</h5>
+                <h1>Gilles Christian Kamgaing K.</h1>
+                <h5 className='text-light'>Software Engineer </h5>
+                <CTA/>
+                <HeaderSocial/> 
+                <div className='my_image'>
+                    <img src={me} alt='My_Image'/> 
+                </div>
+                <a href='#contact' className='scroll__down'>Scroll down</a>
+            </div>
+        </header>
+    )
+}
+
