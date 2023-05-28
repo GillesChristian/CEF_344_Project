cb6d365fab68b1a46fbfd8ad60bea542c3dada05
GillesChristian <christiank.kamgaing@gmail.com>
2023-05-28 06:48:59 +0100

    Testimonial added to Portfolio

--- portfolio./src/App.jsx
+++ portfolio./src/App.jsx
@@ -4,6 +4,7 @@ import Header from './components/header/Header';
 import Navbar from './components/navbar/Navbar';
 import About from './components/about/About' ;
 import Services from './components/services/Services';
+import Testimonial from './components/testimonial/Testimonial'
 import Contact from './components/contact/Contact';
 import Footer from './components/footer/Footer';
 
@@ -16,6 +17,7 @@ function App() {
                 <Navbar/>
                 <About/>
                 <Services />
+                <Testimonial />
                 <Contact />
                 <Footer />
              
