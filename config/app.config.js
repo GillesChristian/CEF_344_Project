30048fba4848dcfefe07358b9820aadac185d1ed
GillesChristian <christiank.kamgaing@gmail.com>
2023-06-23 06:35:52 +0100

    BACKEND

---
+++ config/app.config.js
@@ -0,0 +1,9 @@
+require("dotenv").config;
+
+const MONGO_DB_CONFIG = {
+   DB: 'mongodb+srv://christiankkamgaing:wCvsXiUuXgsPOvbD@Backend-Porfolio.jtp2ckm.mongodb.net/Backend-Porfolio?retryWrites=true&w=majority'
+}
+
+module.exports = {
+   MONGO_DB_CONFIG
+}
--- portfolio./config/app.config.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config;
-
-const MONGO_DB_CONFIG = {
-   DB: 'mongodb+srv://christiankkamgaing:wCvsXiUuXgsPOvbD@backend-portfolio.jtp2ckm.mongodb.net/?retryWrites=true&w=majority'
-}
-
-module.exports = {
-   MONGO_DB_CONFIG
-}
